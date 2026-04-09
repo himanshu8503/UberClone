56112a307dfcc9043821e73ac4702aeaa805dbaf
himanshu8503 <himanshudiwakar8503@gmail.com>
2026-04-09 17:37:10 +0530

    backend Setup

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const app = require("./app.js");
+
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+
+
+server.listen(port,() => {
+    console.log(`the server is running on ${port}`)
+});
